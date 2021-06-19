ff1637a0096e0a1e90f6fedbcfe1983a89a5de94
Sachin Srivastava <thiszsachin@gmail.com>
2021-06-20 03:52:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoic2FjaGluYmVzdDE3IiwiYSI6ImNrcTQ5dXMxdTBjNnUyb3IxNzEwbThidW0ifQ.jVMyIOWwg9VxWQdYKf5E0w";
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true,
+});
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude]);
+}
+function errorLocation() {
+  setup([26.773, 82.1458]);
+}
+function setupMap(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 14,
+  });
+
+  const nav = new mapboxgl.NavigationControl();
+  map.addControl(nav);
+
+  var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken,
+  });
+
+  map.addControl(directions, "top-left");
+}
